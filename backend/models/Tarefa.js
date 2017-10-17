259c2d4248478b243bdb53b0bbe8ccbe982c1f54
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2017-10-17 15:55:55 -0200

    Configurado sequelize.

---
+++ backend/models/Tarefa.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function (sequelize, DataTypes) {
+    var Tarefa = sequelize.define('tarefas', {
+        titulo: DataTypes.STRING,
+        descricao: DataTypes.STRING,
+        data_criacao: DataTypes.DATE,
+        concluida: DataTypes.BOOLEAN,
+    });
+
+    Tarefa.associate = function (models) {
+
+    }
+
+    return Tarefa;
+};
